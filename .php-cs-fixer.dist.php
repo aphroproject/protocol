2054378e292eaa2365e5601919415df7b64b80b2
Aphro Project <aphroproject@proton.me>
2023-05-16 14:40:10 +0200

    chore: initial commit

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Aphro Project.
+ * Copyright (C) 2023 Aphro Project<aphroproject@proton.me>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+$header = <<<'EOF'
+    This file is part of Aphro Project.
+    Copyright (C) 2023 Aphro Project<aphroproject@proton.me>
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    EOF;
+
+$finder = PhpCsFixer\Finder::create()
+    ->ignoreDotFiles(false)
+    ->ignoreVCSIgnored(true)
+    ->in(__DIR__)
+;
+
+$config = (new PhpCsFixer\Config())
+    ->setRules([
+        '@PER' => true,
+        'header_comment' => [
+            'header' => $header,
+        ],
+    ])
+    ->setFinder($finder);
+
+return $config;
